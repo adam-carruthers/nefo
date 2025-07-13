d370ba29a0e14d66d80568b50c3391d3d04577c7
adam.carruthers1 <adam.carruthers1@nhs.net>
2025-07-13 14:24:15 +0100

    Make the icon in the Navbar a link to go back home

--- src/pages/index/Navbar.tsx
+++ src/pages/index/Navbar.tsx
@@ -1,13 +1,13 @@
-import icon from '@/assets/icon.svg';
+import icon from "@/assets/icon.svg";
 
 function Navbar() {
   return (
     <nav className="bg-white shadow p-1">
       <div className="max-w-7xl mx-auto px-4">
-        <div className="flex items-center space-x-2">
+        <a href="/" className="flex items-center space-x-2">
           <img src={icon} alt="Nefo" className="h-16 w-16" />
           <h1 className="text-xl font-extrabold text-gray-800">Nefo</h1>
-        </div>
+        </a>
       </div>
     </nav>
   );
